b9e74732a8d57f833a2af54035fc12023caa67e3
Teddy Dumélé <teddy.dumele@gmail.com>
2025-06-28 22:18:45 +0200

    :wrench: adding tailwind + starting homepage

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,8 @@
+export const HomePage = () => {
+    return (
+        <div>
+        <h1 className="text-3xl font-bold underline">Welcome to the Home Page</h1>
+        <p>This is the main page of our application.</p>
+        </div>
+    );
+}
