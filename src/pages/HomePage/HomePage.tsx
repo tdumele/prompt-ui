bc257046782f63cb8c99835d3d78f1ab61d287a5
Teddy Dumélé <teddy.dumele@gmail.com>
2025-06-28 22:50:07 +0200

    🚧 development sidebar

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,12 @@
+import { Sidebar } from "../../components/Sidebar/Sidebar"
+
 export const HomePage = () => {
-    return (
-        <div>
-        <h1 className="text-3xl font-bold underline">Welcome to the Home Page</h1>
-        <p>This is the main page of our application.</p>
+    return (<>
+        <Sidebar />
+        <div className="p-4 sm:ml-64">
+            <h1 className="text-3xl font-bold underline">Welcome to the Home Page</h1>
+            <p>This is the main page of our application.</p>
         </div>
+    </>
     );
 }
