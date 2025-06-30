c6a8002edddcdc5e38bc334d41642e3755005339
Teddy Dumélé <teddy.dumele@gmail.com>
2025-06-30 19:00:27 +0200

    :lipstick: working on sidebar with grid

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,12 +1,12 @@
 export const Sidebar = () => {
     return (
         <>
-            <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-20 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                    <ul className="space-y-2 font-medium">
+            <aside id="default-sidebar" className="col-span-1 h-screen" aria-label="Sidebar">
+                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-800">
+                    <ul>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <svg className="w-13" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <a href="#" className="flex rounded-lg hover:bg-gray-200">
+                                <svg className="w-9" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M16 12L8 20V44C8 48.4183 11.5817 52 16 52H28L36 60V52H48C52.4183 52 56 48.4183 56 44V20C56 15.5817 52.4183 12 48 12H16Z" fill="#22A381" />
                                     <circle cx="24" cy="32" r="3" fill="white" />
                                     <circle cx="32" cy="32" r="3" fill="white" />
@@ -17,9 +17,9 @@ export const Sidebar = () => {
                     </ul>
                 </div>
             </aside>
-            <aside id="content-sidebar" className="fixed top-0 left-20 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-                <div className="h-full w-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-400 text-center">
-                    <button className="flex items-center p-2 w-58  text-gray-900 rounded-lg dark:text-white bg-gray-700 group"
+            <aside id="content-sidebar" className="col-span-4 h-screen" aria-label="Sidebar">
+                <div className="h-full w-full px-3 py-4 overflow-y-auto bg-gray-50 bg-gray-100 text-center">
+                    <button className="flex items-center p-2 w-full text-gray-900 rounded-lg dark:text-white bg-gray-700 group"
                                 onClick={() => alert('New Prompt')}>
                                     New conversation
                             </button>
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,11 +2,14 @@ import { Sidebar } from "../../components/Sidebar/Sidebar"
 
 export const HomePage = () => {
     return (<>
-        <Sidebar />
-        <div className="p-4 sm:ml-64">
-            <h1 className="text-3xl font-bold underline">Welcome to the Home Page</h1>
-            <p>This is the main page of our application.</p>
+        <div className="grid grid-cols-24">
+            <Sidebar />
+            <main id="content" className="px-4 py-4 col-start-6 col-end-24 h-screen">
+                <h1 className="text-3xl font-bold underline">Welcome to the Home Page</h1>
+                <p>This is the main page of our application.</p>
+            </main>
         </div>
+
     </>
     );
 }
